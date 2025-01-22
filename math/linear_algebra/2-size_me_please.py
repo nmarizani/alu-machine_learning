e9c3183863ea8c1b90116de377c1caf3e154eb69
nmarizani <n.marizani@alustudent.com>
2025-01-22 12:43:55 -0800

    Shape calculation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+"""
+This module defines a function to calculate the shape of a matrix.
+
+Functions:
+    - matrix_shape(matrix): Calculates the shape of a given matrix.
+"""
+
 def matrix_shape(matrix):
-    """Calculates the shape of a matrix.
-    
+    """Calculates the shape of a matrix
+
     Args:
         matrix: The matrix to determine the shape of.
-    
+
     Returns:
         A list of integers representing the shape of the matrix.
     """
