ffbb5bde1dbeca1e7f431761afe97f1da6495814
nmarizani <n.marizani@alustudent.com>
2025-01-22 13:40:08 -0800

    Matrice Addition

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -24,5 +24,4 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1,
        row2 in zip(mat1, mat2)):
         return None
-    return [[row1[i] + row2[i] for i in range(len(row1))] for row1,
-                               row2 in zip(mat1, mat2)]
+    return [[row1[i] + row2[i] for i in range(len(row1))] for row1, row2 in zip(mat1, mat2)]
