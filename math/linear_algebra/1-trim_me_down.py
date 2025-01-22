24f9536421a740c80dc5bd3495c4b3616d6222e5
nmarizani <n.marizani@alustudent.com>
2025-01-22 08:22:38 -0800

    Trimming

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []  # Initialize as an empty list
-the_middle = [[row[2], row[3]] for row in matrix]  # Extract 3rd and 4th columns
+the_middle = [[row[2], row[3]] for row in matrix]  # Extract 3rd and 4th column
 print("The middle columns of the matrix are: {}".format(the_middle))
