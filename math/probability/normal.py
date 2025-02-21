5e2fd3432c153d519a9f600406148219128b6bb2
nmarizani <n.marizani@alustudent.com>
2025-02-21 07:44:42 -0800

    Normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -94,3 +94,30 @@ class Normal:
         exponent = e ** (-((x - self.mean) ** 2) / (2 * self.stddev ** 2))
 
         return coefficient * exponent
+
+    def erf(self, z):
+        """
+        Approximates the error function (erf) using a numerical approximation.
+
+        Formula: 
+        erf(z) ≈ (2 / sqrt(π)) * [ z - (z^3 / 3) + (z^5 / 10) 
+        - (z^7 / 42) + (z^9 / 216) ]
+        """
+        pi = 3.1415926536
+        term1 = z
+        term2 = -(z ** 3) / 3
+        term3 = (z ** 5) / 10
+        term4 = -(z ** 7) / 42
+        term5 = (z ** 9) / 216
+        
+        return (2 / (pi ** 0.5)) * (term1 + term2 + term3 + term4 + term5)
+
+    def cdf(self, x):
+        """
+        Calculates the cumulative distribution function (CDF) value for given x
+
+        Formula:
+        CDF(x) = (1 / 2) * [1 + erf((x - μ) / (σ sqrt(2)))]
+        """
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        return 0.5 * (1 + self.erf(z))
