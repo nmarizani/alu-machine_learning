34e6825be309d3f073c805696f237f6d5a731f47
nmarizani <n.marizani@alustudent.com>
2025-02-21 07:59:16 -0800

    Binomial

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -33,3 +33,31 @@ class Binomial:
 
             self.n = n_estimate
             self.p = mean / self.n
+
+    def factorial(self, num):
+        """Computes factorial of a number (num!) without importing math module."""
+        if num == 0 or num == 1:
+            return 1
+        result = 1
+        for i in range(2, num + 1):
+            result *= i
+        return result
+
+    def pmf(self, k):
+        """
+        Calculates the Probability Mass Function (PMF) for given number success
+        """
+        k = int(k)  # Convert k to an integer if it's not
+
+        if k < 0 or k > self.n:
+            return 0  # k is out of range
+
+        # Compute binomial coefficient: C(n, k) = n! / (k!(n-k)!)
+        n_fact = self.factorial(self.n)
+        k_fact = self.factorial(k)
+        nk_fact = self.factorial(self.n - k)
+
+        comb = n_fact / (k_fact * nk_fact)
+
+        # Compute PMF: P(k) = C(n, k) * p^k * (1 - p)^(n - k)
+        return comb * (self.p ** k) * ((1 - self.p) ** (self.n - k))
