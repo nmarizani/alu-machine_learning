402e6f3d1fac5090b6d77538017630ea20598aa0
nmarizani <n.marizani@alustudent.com>
2025-05-26 04:42:18 -0700

    Training

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
-"""Train a neural network classifier"""
+"""Trains a neural network classifier using TensorFlow"""
 
 import tensorflow as tf
-
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid,
-          layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """Trains a neural network and saves the model"""
+          layer_sizes, activations, alpha, iterations,
+          save_path="/tmp/model.ckpt"):
+    """
+    Builds, trains, and saves a neural network classifier.
 
-    # Reset the graph
-    tf.reset_default_graph()
+    Args:
+        X_train: training input data
+        Y_train: training labels
+        X_valid: validation input data
+        Y_valid: validation labels
+        layer_sizes: list with number of nodes in each layer
+        activations: list of activation functions for each layer
+        alpha: learning rate
+        iterations: number of iterations for training
+        save_path: path to save the trained model
 
-    # Placeholders
+    Returns:
+        The path where the model was saved
+    """
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-
-    # Forward propagation
     y_pred = forward_prop(x, layer_sizes, activations)
-
-    # Loss and accuracy
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
-
-    # Training operation
     train_op = create_train_op(loss, alpha)
 
     # Add to collections
@@ -38,25 +43,23 @@ def train(X_train, Y_train, X_valid, Y_valid,
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('train_op', train_op)
 
-    # Session to run training
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(init)
-
         for i in range(iterations + 1):
-            t_cost, t_accuracy = sess.run([loss, accuracy],
-                                          feed_dict={x: X_train, y: Y_train})
-            v_cost, v_accuracy = sess.run([loss, accuracy],
-                                          feed_dict={x: X_valid, y: Y_valid})
+            t_cost, t_acc = sess.run((loss, accuracy),
+                                     feed_dict={x: X_train, y: Y_train})
+            v_cost, v_acc = sess.run((loss, accuracy),
+                                     feed_dict={x: X_valid, y: Y_valid})
 
             if i % 100 == 0 or i == iterations:
                 print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(t_cost))
-                print("\tTraining Accuracy: {}".format(t_accuracy))
+                print("\tTraining Accuracy: {}".format(t_acc))
                 print("\tValidation Cost: {}".format(v_cost))
-                print("\tValidation Accuracy: {}".format(v_accuracy))
+                print("\tValidation Accuracy: {}".format(v_acc))
 
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
