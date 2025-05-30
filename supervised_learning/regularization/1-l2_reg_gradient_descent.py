be217a4952eafd789e0bda17012021543e0e2ee7
nmarizani <n.marizani@alustudent.com>
2025-05-30 04:35:51 -0700

    Gradient descent

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Updates the weights and biases of a neural network using
-gradient descent with L2 regularization
+Gradient Descent with L2 Regularization for a Neural Network using tanh and softmax activations.
 """
 
 import numpy as np
@@ -13,29 +12,31 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     gradient descent with L2 regularization.
 
     Parameters:
-    - Y (ndarray): one-hot array of shape (classes, m), true labels
-    - weights (dict): weights and biases of the network
-    - cache (dict): activations from forward propagation
+    - Y (ndarray): one-hot array of shape (classes, m) with correct labels
+    - weights (dict): weights and biases of the neural network
+    - cache (dict): outputs of each layer of the neural network
     - alpha (float): learning rate
     - lambtha (float): L2 regularization parameter
-    - L (int): number of layers
+    - L (int): number of layers of the network
+
+    Updates:
+    - weights (dict): updated in-place
     """
     m = Y.shape[1]
-    dZ = cache["A{}".format(L)] - Y
+    dz = cache['A' + str(L)] - Y
 
-    for i in reversed(range(1, L + 1)):
-        A_prev = cache["A{}".format(i - 1)]
-        W_key = "W{}".format(i)
-        b_key = "b{}".format(i)
+    for l in reversed(range(1, L + 1)):
+        A_prev = cache['A' + str(l - 1)]
+        W = weights['W' + str(l)]
+        b = weights['b' + str(l)]
 
-        # Gradient for weights with L2 regularization
-        dW = (1 / m) * (np.matmul(dZ, A_prev.T) + lambtha * weights[W_key])
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        dW = (1 / m) * np.matmul(dz, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
         # Update weights and biases
-        weights[W_key] -= alpha * dW
-        weights[b_key] -= alpha * db
+        weights['W' + str(l)] = W - alpha * dW
+        weights['b' + str(l)] = b - alpha * db
 
-        if i > 1:
-            dA_prev = np.matmul(weights[W_key].T, dZ)
-            dZ = dA_prev * (1 - np.power(A_prev, 2))  # Derivative of tanh
+        # Compute dz for the next layer (if not the input layer)
+        if l > 1:
+            dz = np.matmul(W.T, dz) * (1 - np.square(A_prev))
