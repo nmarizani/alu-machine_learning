8014d6ed20aae32c848ed5a55ea96eb2cb0d2dc7
nmarizani <n.marizani@alustudent.com>
2025-05-30 07:06:25 -0700

    Dropout forward prop

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,47 +1,48 @@
 #!/usr/bin/env python3
-"""
-Forward propagation with Dropout regularization.
-"""
+"""Forward propagation with dropout"""
 
 import numpy as np
 
 
+def softmax(Z):
+    """Compute softmax values for each set of scores in Z"""
+    exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+    return exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+
+
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
-    Conducts forward propagation using Dropout.
+    Conducts forward propagation using Dropout
 
-    Parameters:
-    - X: numpy.ndarray of shape (nx, m), input data
-    - weights: dictionary of weights and biases of the neural network
-    - L: number of layers in the network
-    - keep_prob: probability that a node will be kept
+    Arguments:
+    X -- input data of shape (nx, m)
+    weights -- dictionary of weights and biases
+    L -- number of layers
+    keep_prob -- probability of keeping a node active
 
     Returns:
-    - cache: dictionary containing outputs of each layer and dropout masks
+    cache -- dictionary containing the outputs of each layer and the dropout
     """
     cache = {}
     cache['A0'] = X
 
-    for l in range(1, L + 1):
-        Wl = weights['W' + str(l)]
-        bl = weights['b' + str(l)]
-        A_prev = cache['A' + str(l - 1)]
+    for layer in range(1, L + 1):
+        W = weights['W' + str(layer)]
+        b = weights['b' + str(layer)]
+        A_prev = cache['A' + str(layer - 1)]
+        Z = np.matmul(W, A_prev) + b
 
-        Zl = np.matmul(Wl, A_prev) + bl
-
-        if l == L:
-            # Softmax for the output layer
-            exp_Z = np.exp(Zl - np.max(Zl, axis=0, keepdims=True))
-            Al = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
-            cache['A' + str(l)] = Al
+        if layer == L:
+            # Output layer with softmax
+            A = softmax(Z)
         else:
-            # tanh activation
-            Al = np.tanh(Zl)
-            # Dropout mask
-            Dl = np.random.rand(*Al.shape) < keep_prob
-            Al *= Dl
-            Al /= keep_prob
-            cache['A' + str(l)] = Al
-            cache['D' + str(l)] = Dl
+            # Hidden layers with tanh and dropout
+            A = np.tanh(Z)
+            D = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
+            A = A * D
+            A /= keep_prob
+            cache['D' + str(layer)] = D
+
+        cache['A' + str(layer)] = A
 
     return cache
