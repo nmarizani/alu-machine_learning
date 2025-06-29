5e896f8d226e297a3ea709beebe8f4fd52b822a0
nmarizani <n.marizani@alustudent.com>
2025-06-29 12:41:22 -0700

    Sentience

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -9,7 +9,7 @@ def sentientPlanets():
     Fetches the list of home planet names for all sentient species.
 
     Returns:
-        list: A list of planet names.
+        list: A list of planet names (may include 'unknown').
     """
     url = "https://swapi.dev/api/species/"
     planets = []
@@ -20,23 +20,24 @@ def sentientPlanets():
             break
 
         data = response.json()
-        species_list = data.get('results', [])
+        results = data.get('results', [])
 
-        for species in species_list:
+        for species in results:
             classification = species.get('classification', '').lower()
             designation = species.get('designation', '').lower()
 
-            if 'sentient' in [classification, designation]:
+            if classification == 'sentient' or designation == 'sentient':
                 homeworld_url = species.get('homeworld')
                 if homeworld_url:
-                    planet_response = requests.get(homeworld_url)
-                    if planet_response.status_code == 200:
-                        planet_name = planet_response.json().get('name', 'unknown')
-                        planets.append(planet_name)
-                    else:
-                        planets.append('unknown')
+                    try:
+                        planet_response = requests.get(homeworld_url)
+                        if planet_response.status_code == 200:
+                            planet_name = planet_response.json().get('name')
+                            planets.append(planet_name)
+                    except Exception:
+                        planets.append("unknown")
                 else:
-                    planets.append('unknown')
+                    planets.append("unknown")
 
         url = data.get('next')
 
